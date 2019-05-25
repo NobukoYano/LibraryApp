68adf1a82346b9410c46e45e73a9ebcf5b479d6e
NobukoYano <santorini0831@gmail.com>
2019-05-25 16:32:23 +0200

    added static root in urls.py

--- library/urls.py
+++ library/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', books.views.top, name='top'),    
     path('books/', include("books.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
